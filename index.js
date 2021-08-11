cf75e9f86d3372f0e3f684dfa69d228b3eea459e
Daniel <dmilano857@gmail.com>
2021-08-11 16:41:36 -0600

    test

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const port = 5000;
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cors())
+app.use(
+  res.set({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  }))
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
